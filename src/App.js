bcac7dccef51eb4391a5aafb25bcc642f8adce1b
Shivambisht01 <shivambisht252@gmail.com>
2024-09-25 19:21:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoListing from './TodoListing';
+
+function App(){
+    return (
+        <div className="app">
+        <TodoListing/>
+        </div>
+    );
+}
+
+export default App;
